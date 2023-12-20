f6b42aa71729c134adb2116e471ad94d608f860e
Nathrop <chaudenisbreiz@yahoo.fr>
2023-12-20 11:50:56 +0100

    création fonction login.js

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,22 +1,61 @@
+/* login.js */
+import { login } from './api.js';
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('loginForm').addEventListener('submit', async (event) => {
+        event.preventDefault();
 
-// Récupération des informations du formulaire dans ces variables
-const username = document.getElementById('username').value;
-const password = document.getElementById('password').value;
+        // Récupération des informations du formulaire ici
+        const formData = new FormData(event.target); // Récupère les données du formulaire
+        const email = formData.get('email');
+        const password = formData.get('password');
 
-// Crée un objet credentials avec les informations de connexion
-const credentials = {
-    username: username,
-    password: password
-};
+        // Vérification du format de l'adresse e-mail avec une expression régulière
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const isEmailValid = emailRegex.test(email);
+        const emailAlert = document.getElementById('emailAlert');
 
-// Appel de la fonction login du fichier api.js avec les informations d'identification
-login(credentials)
-    .then(response => {
-        // Gère la réponse après la tentative de connexion
-        // Par exemple, redirige l'utilisateur vers une autre page si la connexion est réussie
-    })
-    .catch(error => {
-        // Gère les erreurs survenues lors de la connexion
-        // Utilise showError ou une autre méthode pour afficher les messages d'erreur
-        showError("Mot de passe incorrect");
+        if (!isEmailValid) {
+            emailAlert.style.display = 'block';
+            return;
+        }
+
+        // Vérification du format du mot de passe avec une expression régulière
+        const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+        const isPasswordValid = passwordRegex.test(password);
+        const passwordAlert = document.getElementById('passwordAlert');
+
+        if (!isPasswordValid) {
+            passwordAlert.style.display = 'block';
+            return;
+        }
+
+        // Crée un objet credentials avec les informations de connexion
+        const credentials = {
+            username: email,
+            password: password
+        };
+
+        try {
+            const response = await login(credentials);
+            // Redirection vers la page d'accueil après connexion réussie
+            window.location.href = '/index.html';
+            // Stocke le token dans le localStorage
+            window.localStorage.setItem('token', response.token);
+            // Stocke le token dans le localStorage
+            window.localStorage.setItem('token', response.token);
+            // Nettoie les informations sensibles stockées temporairement après la connexion
+            window.localStorage.removeItem('temporaryUserInformation');
+            // Cache les liens des catégories
+            document.querySelector('.categories').style.display = 'none';
+            // Change le texte du lien "log in" en "log out"
+            document.getElementById('login').textContent = 'Log out';
+
+
+        } catch (error) {
+            // Gère les erreurs survenues lors de la connexion
+            // Utilise showError ou une autre méthode pour afficher les messages d'erreur
+            showError("Mot de passe incorrect");
+        }
     });
+});
+
