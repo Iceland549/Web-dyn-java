c542540557b9252bbfc5784a219686bd96895c7d
Nathrop <chaudenisbreiz@yahoo.fr>
2023-12-30 08:56:33 +0100

    modification createElement en button

--- FrontEnd/js/factory.js
+++ FrontEnd/js/factory.js
@@ -5,6 +5,7 @@ export function createWorkCard(work) {
   const article = document.createElement("article");
   article.classList.add("work");
   article.setAttribute("data-category", work.category.id);
+  article.id = `work-${work.id}`
 
   const imageElement = document.createElement("img");
   imageElement.src = work.imageUrl;
@@ -21,9 +22,8 @@ export function createWorkCard(work) {
 
 // Fonction pour créer des boutons catégories
 export function createCategoryButtons(category) {
-  const categoryElement = document.createElement("a");
+  const categoryElement = document.createElement("button");
   categoryElement.textContent = category.name;
-  categoryElement.href = "#";
   categoryElement.classList.add("categories-link");
 
   return categoryElement;
